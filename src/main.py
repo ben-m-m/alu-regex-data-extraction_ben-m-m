42af217adb3920b32a8537d93b2b4038103f9798
ben-m-m <mainaben23@gmail.com>
2026-05-22 23:51:22 +0200

    Refactor data extraction logic and update output format for sensitive information

--- src/main.py
+++ src/main.py
@@ -3,22 +3,31 @@
 import re
 import json
 
-email_address = re.compile(r'\b[a-zA-Z0-9._%+-]+@(?:alueducation\.com|alumni\.alueducation\.com|si\.alueducation\.com)\b')
+email_address_pattern = re.compile(r'\b[a-zA-Z0-9._%+-]+@(?:alueducation\.com|alumni\.alueducation\.com|si\.alueducation\.com)\b')
 url_pattern = re.compile(r'\bhttps?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[^\s]*)?\b')
 phone_no_pattern = re.compile(r'(?:\+\d{1,3}[- ]?)?(?:\(?\d{2,4}\)?[- ]?)?\d{3}[- ]?\d{3,4}')
 credit_card_pattern = re.compile(r'\b(?:\d{4}[- ]?){3}\d{4}\b')
 
+def hidden_credit_card_numbers(credit_cards):
+    hidden_cards = []
+    digits = re.sub(r'\D', '', credit_cards)
+    if len(digits) == 16:
+        hidden_card = '**** **** **** ' + digits[-4:]
+        hidden_cards.append(hidden_card)
+    return hidden_cards[0] if hidden_cards else ""
+
+
 def extract_sensitive_info(text):
-    emails = email_address.findall(text)
-    urls = url_pattern.findall(text)
-    phone_numbers = phone_no_pattern.findall(text)
-    credit_cards = credit_card_pattern.findall(text)
+    emails = list(set(email_address_pattern.findall(text)))
+    urls = list(set(url_pattern.findall(text)))
+    phone_numbers = list(set(phone_no_pattern.findall(text)))
+    credit_cards_lst = list(set(credit_card_pattern.findall(text)))
 
     return {
         'emails': emails,
         'urls': urls,
         'phone_numbers': phone_numbers,
-        'credit_cards': credit_cards
+        'credit_cards': [hidden_credit_card_numbers(card) for card in credit_cards_lst]
     }
 
 if __name__ == "__main__":
@@ -29,3 +38,5 @@ if __name__ == "__main__":
 
     with open('../output/sample-output.json', 'w') as json_file:
         json.dump(sensitive_info, json_file, indent=4)
+    
+    print("Sensitive information extracted and saved to sample-output.json")
